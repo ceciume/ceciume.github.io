13e32fa92b8ac3fe3ff4dc35bf4847a4620addad
ceciume <3214560024@qq.com>
2023-05-09 12:27:30 +0800

    navbar以及矿床查询

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,32 +1,39 @@
 <template>
   <v-app-bar app
              color="#e5f4f9">
-    <v-toolbar-title>U资源样本库</v-toolbar-title>
+    <v-toolbar-title></v-toolbar-title>
     <v-spacer></v-spacer>
-    <router-link to="/home" class="my-link"><v-btn text>
+    <router-link to="/home"
+                 class="my-link"><v-btn text>
         <v-avatar image="src\assets\iconfont\首页.png"
                   size="30"></v-avatar>
         首页
       </v-btn></router-link>
-    <router-link to="/mineral-query" class="my-link"><v-btn text>
+    <router-link to="/mineral-query"
+                 class="my-link"><v-btn text>
         <v-avatar image="src\assets\iconfont\矿山.png"
                   size="30"></v-avatar>矿床查询</v-btn></router-link>
-    <router-link to="/dataset-query" class="my-link"><v-btn text>
+    <router-link to="/dataset-query"
+                 class="my-link"><v-btn text>
         <v-avatar image="src\assets\iconfont\遥感卫星.png"
-                  size="30"></v-avatar>数据集查询</v-btn></router-link>
-    <router-link to="/about-us" class="my-link">
+                  size="30"></v-avatar>查询</v-btn></router-link>
+    <router-link to="/about-us"
+                 class="my-link">
       <v-btn text>
         <v-avatar image="src\assets\iconfont\关于我们.png"
                   size="30"></v-avatar>关于我们
       </v-btn>
     </router-link>
-    <router-link to="/order-list" class="my-link"><v-btn text>
+    <router-link to="/order-list"
+                 class="my-link"><v-btn text>
         <v-avatar image="src\assets\iconfont\订单列表.png"
                   size="30"></v-avatar>订单列表</v-btn></router-link>
-    <router-link to="/order-review" class="my-link"><v-btn text>
+    <router-link to="/order-review"
+                 class="my-link"><v-btn text>
         <v-avatar image="src\assets\iconfont\订单审核.png"
                   size="30"></v-avatar>订单审核</v-btn></router-link>
-    <router-link to="/user-tutorial" class="my-link">
+    <router-link to="/user-tutorial"
+                 class="my-link">
       <v-btn text>
         <v-avatar image="src\assets\iconfont\用户手册.png"
                   size="30"></v-avatar>用户手册</v-btn>
@@ -34,20 +41,84 @@
 
     <v-spacer></v-spacer>
     <v-btn text
-           @click="$router.push('/')">Logout</v-btn>
+           @click="$router.push('/')">
+      <img src="src\assets\iconfont\设置.png"
+           width="24"
+           alt="icon" />
+    </v-btn>
     <v-avatar image="src\assets\iconfont\头像.png"
-              size="30"></v-avatar>
+              size="30"
+              @click="showMenu = !showMenu"></v-avatar>
+    <div>
+      <v-menu v-model="showMenu"
+              class="person-menu"
+              :close-on-content-click="false">
+        <v-list>
+          <v-list-item @click="goToProfile">
+            <v-list-item-title>个人中心</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="goToOrders">
+            <v-list-item-title>订单列表</v-list-item-title>
+          </v-list-item>
+          <v-list-item @click="logOut">
+            <v-list-item-title>退出登录</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+    <v-dialog v-model="dialog"
+              width="auto">
+      <v-card>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn color="primary"
+                 block
+                 @click="dialog = false">Close Dialog</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-app-bar>
 </template>
 
 <script>
 export default {
   name: "NavBar",
+  data () {
+    return {
+      showMenu: false,
+      dialog: false,
+    };
+  },
+  methods: {
+    goToProfile () {
+      // 跳转到个人中心页面的代码
+      this.dialog = true;
+    },
+    goToOrders () {
+      // 跳转到订单列表页面的代码
+    },
+    logOut () {
+      // 退出登录的代码
+      this.$router.push('/')
+    }
+  },
+
 };
 </script>
 <style scoped>
+.parent-element {
+  position: relative;
+}
+
 .my-link {
   color: rgb(34, 34, 34); /* 字体颜色 */
   text-decoration: none; /* 取消下划线 */
+}
+.person-menu {
+  position: absolute !important;
+  top: 50px;
+  left: 1310px;
 }
 </style>
--- src/views/MineralQuery.vue
+++ src/views/MineralQuery.vue
@@ -52,6 +52,32 @@
       <!-- 地图控件 -->
       <div ref="map"
            class="map"></div>
+      <!-- 地点描述卡片 -->
+      <v-card class="des-card"
+              v-if="selectedPlace">
+        <v-card-title>{{ selectedPlace?.title }}</v-card-title>
+        <v-img :src="selectedPlace?.imageSrc"
+               height="100px"></v-img>
+
+        <v-card-text>{{ selectedPlace.description }}
+          <div v-if="selectedPlace.relatedLinks">
+            相关链接：
+            <v-btn v-for="(link, index) in selectedPlace.relatedLinks.slice(0, 3)"
+                   :key="index"
+                   text
+                   small
+                   :href="link.url"
+                   target="_blank">
+              {{ link.title }}
+            </v-btn>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text
+                 :href="selectedPlace.link"
+                 target="_blank">了解更多</v-btn>
+        </v-card-actions>
+      </v-card>
     </v-card>
   </div>
 </template>
@@ -105,6 +131,7 @@ export default {
           },
           description: '位于太平洋中部的热带天堂，拥有美丽的珊瑚礁和海滩。',
           link: 'https://zh.wikipedia.org/wiki/%E5%B8%95%E5%8A%B3%E7%BE%A4%E5%B2%9B'
+          , relatedLinks: []
         },
         {
           name: '埃菲尔铁塔',
@@ -115,6 +142,7 @@ export default {
           },
           description: '位于法国巴黎市的地标性建筑，高324米，是法国最著名的旅游景点之一。',
           link: 'https://zh.wikipedia.org/wiki/%E5%9F%83%E8%8F%B2%E5%B0%94%E9%93%81%E5%A1%94'
+          , relatedLinks: []
         },
         {
           name: '金门大桥',
@@ -125,6 +153,7 @@ export default {
           },
           description: '位于美国加利福尼亚州旧金山湾区，是世界上最长的悬索桥之一。',
           link: 'https://zh.wikipedia.org/wiki/%E9%87%91%E9%96%80%E5%A4%A7%E6%A9%8B'
+          , relatedLinks: []
         },
         {
           name: '普罗旺斯',
@@ -135,6 +164,7 @@ export default {
           },
           description: '法国南部地区，以其美丽的薰衣草田和酒庄而闻名。',
           link: 'https://zh.wikipedia.org/wiki/%E6%99%AE%E7%BD%97%E6%97%BA%E6%96%AF'
+          , relatedLinks: []
         },
         {
           name: '乞力马扎罗山',
@@ -145,6 +175,7 @@ export default {
           },
           description: '坐落于坦桑尼亚的山脉，是非洲最高峰，同时也是世界七大高峰之一。',
           link: 'https://zh.wikipedia.org/wiki/%E4%B9%9E%E5%8A%9B%E9%A9%AC%E6%89%8E%E7%BD%97%E5%B1%B1'
+          , relatedLinks: []
         },
         {
           name: "玄武湖",
@@ -155,6 +186,7 @@ export default {
           },
           description: "玄武湖位于南京市中心，是南京市著名的风景区和旅游景点。",
           link: "https://zh.wikipedia.org/wiki/%E7%8E%84%E6%AD%A6%E6%B9%96"
+          , relatedLinks: []
         },
         {
           name: "苏州园林",
@@ -164,7 +196,15 @@ export default {
             lng: 120.585
           },
           description: "苏州园林，是中国苏州市的园林文化遗产。苏州园林是苏州园林文化的代表，是江南园林的瑰宝。",
-          link: "https://zh.wikipedia.org/wiki/%E8%8B%8F%E5%B7%9E%E5%9B%AD%E6%9E%97"
+          link: "https://zh.wikipedia.org/wiki/%E8%8B%8F%E5%B7%9E%E5%9B%AD%E6%9E%97",
+          relatedLinks: [{
+            title: '维基百科',
+            url: 'https://zh.wikipedia.org/wiki/%E5%9F%83%E8%8F%B2%E5%B0%94%E9%93%81%E5%A1%94',
+          },
+          {
+            title: '官方网站',
+            url: 'https://www.toureiffel.paris/',
+          },]
         },
         {
           name: "黄山",
@@ -175,6 +215,7 @@ export default {
           },
           description: "黄山位于安徽省南部，是中国著名的风景区、文化区、世界文化与自然遗产。",
           link: "https://zh.wikipedia.org/wiki/%E9%BB%84%E5%B1%B1"
+          , relatedLinks: []
         },
         {
           name: "泰山",
@@ -185,6 +226,7 @@ export default {
           },
           description: "泰山位于中国山东省泰安市境内，是中国著名的名山之一，素有五岳之首的美誉。",
           link: "https://zh.wikipedia.org/wiki/%E6%B3%B0%E5%B1%B1"
+          , relatedLinks: []
         },
         {
           name: "故宫",
@@ -195,6 +237,7 @@ export default {
           },
           description: "故宫是明清两代的皇宫，位于中国北京市中心的故宫广场上，是中国明清两代的宫廷建筑群，也是世界上现存规模最大、保存最完整的木质结构古建筑之一。",
           link: "https://zh.wikipedia.org/wiki/%E6%95%85%E5%AE%AB"
+          , relatedLinks: []
         }
 
       ],
@@ -224,7 +267,8 @@ export default {
           icon: 'marker',
           description: place.description,
           link: place.link,
-          imageSrc: place.imageSrc
+          imageSrc: place.imageSrc,
+          relatedLinks: place.relatedLinks
         }
       };
     });
@@ -254,18 +298,18 @@ export default {
 
       // 注册 click 事件处理程序
       this.map.on('click', 'places', (e) => {
-        // 获取被单击的要素
-        const feature = e.features[0];
-        const dCard = `<v-card style="z-index: 1; opacity: 0.8; width: 350px; border-radius: 10px;">
-      <v-card-title>${feature.properties.title}</v-card-title>
-      <v-card-text>${feature.properties.description}</v-card-text>
-    </v-card>`;
-        // 创建弹出卡片
-        const popup = new mapboxgl.Popup({ offset: [10, -10] })
-          .setLngLat(feature.geometry.coordinates)
-          .setHTML(dCard);
-        // 将卡片与地图上的点关联起来
-        popup.addTo(this.map);
+        // 获取当前点击位置的要素
+        const features = this.map.queryRenderedFeatures(e.point);
+        // 检查 e.features 数组的长度
+        if (features.length === 0) {
+          // 如果 e.features 数组的长度为 0，将 selectedPlace 变量的值设置为 null
+          this.selectedPlace = null;
+        } else {
+          // 否则，获取被单击的要素
+          const feature = e.features[0];
+          console.log(feature.properties);
+          this.selectedPlace = feature.properties;
+        }
       });
 
       // 将鼠标指针设置为手型，以表明可点击
@@ -278,9 +322,6 @@ export default {
         this.map.getCanvas().style.cursor = '';
       });
     });
-
-
-
 
 
 
@@ -330,7 +371,23 @@ export default {
   cursor: pointer;
 }
 .des-card {
+  position: absolute;
+  bottom: 30px;
+  right: 50px;
   z-index: 1;
   opacity: 0.9;
+  width: 350px;
+  border-radius: 10px;
+}
+/* 图层选择器 */
+.layer-switcher {
+  position: absolute;
+  top: 20px;
+  right: 20px;
+  z-index: 9999;
+  background-color: #d9d8d8;
+  padding: 10px;
+  border-radius: 4px;
+  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.3);
 }
 </style>
